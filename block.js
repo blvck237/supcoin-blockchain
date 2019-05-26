f416fd30147b007ed0234a1af90bebcd67e58f14
Jordan Fokoua <jfokoua@gmail.com>
2019-05-26 17:55:57 +0200

    :sparkles: Implement genesis method

--- block.js
+++ block.js
@@ -1,9 +1,15 @@
+const { GENESIS_DATA } = require("./config");
+
 class Block {
-  constructor({timestamp, previousHash, hash, data}) {
+  constructor({ timestamp, previousHash, hash, data }) {
     this.timestamp = timestamp;
     this.previousHash = previousHash;
     this.hash = hash;
     this.data = data;
+  }
+
+  static genesis() {
+    return new Block(GENESIS_DATA);
   }
 }
 
