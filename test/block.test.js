d372171bdd59d89b54899d22d24daff4bbba132d
Jordan Fokoua <jfokoua@gmail.com>
2019-05-26 18:58:34 +0200

    :sparkles: Test hash

--- test/block.test.js
+++ test/block.test.js
@@ -1,4 +1,5 @@
 const Block = require("../block");
+const cryptoHash = require("../crypto-hash");
 const { GENESIS_DATA } = require("../config");
 
 describe("Block", () => {
@@ -46,6 +47,12 @@ describe("Block", () => {
 
     it("sets the `timestamp`", () => {
       expect(minedBlock.timestamp).not.toEqual(undefined);
+    });
+
+    it("creates sha256 based on params", () => {
+      expect(minedBlock.hash).toEqual(
+        cryptoHash(minedBlock.timestamp, lastBlock.hash, data)
+      );
     });
   });
 });
--- test/hash.test.js
+++ test/hash.test.js
@@ -1,13 +1,13 @@
-const hash = require("../hash");
+const cryptoHash = require("../crypto-hash");
 
 describe("hash()", () => {
   it("generates a sha256 key", () => {
-    expect(hash("foo")).toEqual(
+    expect(cryptoHash("foo")).toEqual(
       "2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae"
     );
   });
 
   it("produces the same hash with the same parameters no matter the order", () => {
-    expect(hash("one", "two", "three")).toEqual(hash("three", "two", "one"));
+    expect(cryptoHash("one", "two", "three")).toEqual(cryptoHash("three", "two", "one"));
   });
 });
