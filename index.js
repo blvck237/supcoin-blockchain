2837a35580b2ed4d156d5d36482c2194af911b27
Jordan Fokoua <jfokoua@gmail.com>
2019-05-28 22:11:54 +0200

    :sparkles: Synchronize nodes on connection

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
+// Class imports
 const Blockchain = require("./blockchain");
 const PubSub = require("./utils/pubsub");
 
 const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
+
+const DEFAULT_PORT = 3000;
+let PEER_PORT;
+const ROOT_NOOD_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 setTimeout(() => {
   pubsub.broadcastChain();
@@ -25,8 +31,19 @@ app.post("/supcoin/mineblock", (req, res) => {
   res.redirect("/supcoin/blocks");
 });
 
-const DEFAULT_PORT = 3000;
-let PEER_PORT;
+const syncNodeChains = () => {
+  console.log("Log: syncNodeChains -> syncNodeChains")
+  request(
+    { url: `${ROOT_NOOD_ADDRESS}/supcoin/blocks` },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const rootChain = JSON.parse(body);
+        console.log("Log: syncNodeChains -> rootChain", rootChain);
+        blockchain.replaceChain(rootChain);
+      }
+    }
+  );
+};
 
 if (process.env.GENERATE_PEER_PORT == "true") {
   PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
@@ -36,4 +53,5 @@ const PORT = PEER_PORT || DEFAULT_PORT;
 
 app.listen(PORT, () => {
   console.info(`Listening on port ${PORT}`);
+  syncNodeChains();
 });
