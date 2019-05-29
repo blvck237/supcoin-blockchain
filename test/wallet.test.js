be1718a7d1543634826039f3029006f607482183
Jordan Fokoua <jfokoua@gmail.com>
2019-05-29 23:14:57 +0200

    :sparkles: Implement make transaction from wallet and its test

--- test/wallet.test.js
+++ test/wallet.test.js
@@ -1,4 +1,5 @@
 const Wallet = require("../wallet/index");
+const Transaction = require("../wallet/transaction");
 const { verifySignature } = require("../utils");
 
 describe("Wallet", () => {
@@ -35,6 +36,38 @@ describe("Wallet", () => {
           signature: new Wallet().sign(data)
         })
       ).toBe(false);
+    });
+  });
+
+  describe("createTransaction", () => {
+    describe("and the amount exceeds the balance", () => {
+      it("throws an error", () => {
+        expect(() =>
+          wallet.createTransaction({ amount: 9999999, receiver: "Jojo" })
+        ).toThrow("Amount exceeds balance");
+      });
+    });
+
+    describe("and the amount is valid", () => {
+      let transaction, amount, receiver;
+
+      beforeEach(() => {
+        amount = 50;
+        receiver = "Jojo";
+        transaction = wallet.createTransaction({ amount, receiver });
+      });
+
+      it("creates an instance of `Transaction`", () => {
+        expect(transaction instanceof Transaction).toBe(true);
+      });
+
+      it("matches transaction input in wallet", () => {
+        expect(transaction.input.address).toEqual(wallet.publicKey);
+      });
+
+      it("outputs the amount of the receiver", () => {
+        expect(transaction.outputMap[receiver]).toEqual(amount);
+      });
     });
   });
 });
--- wallet/index.js
+++ wallet/index.js
@@ -1,3 +1,4 @@
+const Transaction = require("./transaction");
 const { INITIAL_BALANCE } = require("../config");
 const { ec } = require("../utils");
 const { cryptoHash } = require("../utils");
@@ -13,6 +14,13 @@ class Wallet {
 
   sign(data) {
     return this.keyPair.sign(cryptoHash(data));
+  }
+
+  createTransaction({ receiver, amount }) {
+    if (amount > this.balance) {
+      throw new Error("Amount exceeds balance");
+    }
+    return new Transaction({senderWallet: this, receiver, amount});
   }
 }
 
