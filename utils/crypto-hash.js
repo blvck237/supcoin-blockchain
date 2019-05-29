1894f70c51595cb2f477696bf638b16f7f89f3f6
Jordan Fokoua <jfokoua@gmail.com>
2019-05-29 23:13:31 +0200

    :zap: Improve crypto has algorithm

--- utils/crypto-hash.js
+++ utils/crypto-hash.js
@@ -3,7 +3,7 @@ const crypto = require("crypto");
 const cryptoHash = (...inputs) => { //we are combining our inputs into a single array
   const hash = crypto.createHash("sha256");
 
-  hash.update(inputs.sort().join(" ")); // sort is to reorder parameters alphabetically spreaded in 'inputs' and join is to concatenate params. This is to allow our function return the same result no matter the order of the params
+  hash.update(inputs.map(input => JSON.stringify(input)).sort().join(" ")); // sort is to reorder parameters alphabetically spreaded in 'inputs' and join is to concatenate params. This is to allow our function return the same result no matter the order of the params
   return hash.digest("hex");
 };
 
