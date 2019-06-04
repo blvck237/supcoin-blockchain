5d688ca1b4e96c4fd9217541b0e57ae384de099d
Jordan Fokoua <jfokoua@gmail.com>
2019-06-04 17:22:25 +0200

    :zap: Set genesis data

--- config.js
+++ config.js
@@ -1,12 +1,14 @@
+const { cryptoHash } = require("./utils");
+
 const INITIAL_DIFFICULTY = 3;
 const MINE_RATE = 1000;
 const INITIAL_BALANCE = 1000;
 const GENESIS_DATA = {
-  timestamp: 1,
-  previousHash: "-----",
-  hash: "hash-one",
+  timestamp: new Date().getTime(),
+  previousHash: "----",
+  hash: cryptoHash("hash one"),
   difficulty: INITIAL_DIFFICULTY,
-  nonce: 0,
+  nonce: 1,
   data: []
 };
 const REWARD_INPUT = { address: "*authorized-reward*" };
